28401d48dab0517b4bb1aef55d6bda3aea449896
Peter Schlömer (dapete) <dev@dapete.net>
2025-12-14 21:35:02 +0100

    JSpecify: change to NullMarked

--- src/main/java/net/dapete/exceptional/WrappedExceptionalException.java
+++ src/main/java/net/dapete/exceptional/WrappedExceptionalException.java
@@ -1,15 +1,16 @@
 package net.dapete.exceptional;
 
-import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.NullMarked;
 
 /**
  * Exception that wraps exceptions returned by the lambdas in the {@link net.dapete.exceptional.function} package.
  * <p>
  * Used by {@link ExceptionalWrapper}.
  */
+@NullMarked
 public class WrappedExceptionalException extends RuntimeException {
 
-    WrappedExceptionalException(@NonNull Exception cause) {
+    WrappedExceptionalException(Exception cause) {
         super(cause);
     }
 
