92c9229b66214dd7283cf38079726336a40ffa2f
Peter Schlömer (dapete) <dev@dapete.net>
2025-12-22 18:40:34 +0100

    Use explicit try/catch block for all wrappers

--- src/main/java/net/dapete/exceptional/ExceptionalWrapper.java
+++ src/main/java/net/dapete/exceptional/ExceptionalWrapper.java
@@ -42,16 +42,58 @@ public final class ExceptionalWrapper {
         };
     }
 
+    /**
+     * Turns a lambda with exceptions into one that throws a {@link WrappedExceptionalException} if an exception of type {@link E} was thrown.
+     *
+     * @param biConsumer lambda with exceptions
+     * @param <T>        the type of the object argument to the operation
+     * @param <E>        the type of exception thrown by {@code biConsumer}
+     * @return a lambda that throws a {@link WrappedExceptionalException} if an exception of type {@link E} was thrown
+     */
     public static <T, E extends Exception> @NonNull ObjDoubleConsumer<T> wrap(@NonNull ExceptionalObjDoubleConsumer<? super T, E> biConsumer) {
-        return wrap((ExceptionalBiConsumer<T, Double, E>) biConsumer::accept)::accept;
+        return (t, u) -> {
+            try {
+                biConsumer.accept(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
+    /**
+     * Turns a lambda with exceptions into one that throws a {@link WrappedExceptionalException} if an exception of type {@link E} was thrown.
+     *
+     * @param biConsumer lambda with exceptions
+     * @param <T>        the type of the object argument to the operation
+     * @param <E>        the type of exception thrown by {@code biConsumer}
+     * @return a lambda that throws a {@link WrappedExceptionalException} if an exception of type {@link E} was thrown
+     */
     public static <T, E extends Exception> @NonNull ObjIntConsumer<T> wrap(@NonNull ExceptionalObjIntConsumer<? super T, E> biConsumer) {
-        return wrap((ExceptionalBiConsumer<T, Integer, E>) biConsumer::accept)::accept;
+        return (t, u) -> {
+            try {
+                biConsumer.accept(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
+    /**
+     * Turns a lambda with exceptions into one that throws a {@link WrappedExceptionalException} if an exception of type {@link E} was thrown.
+     *
+     * @param biConsumer lambda with exceptions
+     * @param <T>        the type of the object argument to the operation
+     * @param <E>        the type of exception thrown by {@code biConsumer}
+     * @return a lambda that throws a {@link WrappedExceptionalException} if an exception of type {@link E} was thrown
+     */
     public static <T, E extends Exception> @NonNull ObjLongConsumer<T> wrap(@NonNull ExceptionalObjLongConsumer<? super T, E> biConsumer) {
-        return wrap((ExceptionalBiConsumer<T, Long, E>) biConsumer::accept)::accept;
+        return (t, u) -> {
+            try {
+                biConsumer.accept(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     /**
@@ -64,7 +106,8 @@ public final class ExceptionalWrapper {
      * @param <E>        the type of exception thrown by {@code biFunction}
      * @return a lambda that throws a {@link WrappedExceptionalException} if an exception of type {@link E} was thrown
      */
-    public static <T, U, R, E extends Exception> @NonNull BiFunction<T, U, R> wrap(@NonNull ExceptionalBiFunction<? super T, ? super U, ? extends R, E> biFunction) {
+    public static <T, U, R, E extends Exception> @NonNull BiFunction<T, U, R> wrap(
+            @NonNull ExceptionalBiFunction<? super T, ? super U, ? extends R, E> biFunction) {
         return (t, u) -> {
             try {
                 return biFunction.apply(t, u);
@@ -75,21 +118,39 @@ public final class ExceptionalWrapper {
     }
 
     public static <T, U, E extends Exception> @NonNull ToDoubleBiFunction<T, U> wrap(@NonNull ExceptionalToDoubleBiFunction<T, U, E> biFunction) {
-        return wrap((ExceptionalBiFunction<T, U, Double, E>) biFunction::applyAsDouble)::apply;
+        return (t, u) -> {
+            try {
+                return biFunction.applyAsDouble(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <T, U, E extends Exception> @NonNull ToIntBiFunction<T, U> wrap(@NonNull ExceptionalToIntBiFunction<T, U, E> biFunction) {
-        return wrap((ExceptionalBiFunction<T, U, Integer, E>) biFunction::applyAsInt)::apply;
+        return (t, u) -> {
+            try {
+                return biFunction.applyAsInt(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <T, U, E extends Exception> @NonNull ToLongBiFunction<T, U> wrap(@NonNull ExceptionalToLongBiFunction<T, U, E> biFunction) {
-        return wrap((ExceptionalBiFunction<T, U, Long, E>) biFunction::applyAsLong)::apply;
-    }
-
-    public static <T, U, E extends Exception> @NonNull BiPredicate<T, U> wrap(@NonNull ExceptionalBiPredicate<? super T, ? super U, E> biFunction) {
         return (t, u) -> {
             try {
-                return biFunction.test(t, u);
+                return biFunction.applyAsLong(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
+    }
+
+    public static <T, U, E extends Exception> @NonNull BiPredicate<T, U> wrap(@NonNull ExceptionalBiPredicate<? super T, ? super U, E> biPredicate) {
+        return (t, u) -> {
+            try {
+                return biPredicate.test(t, u);
             } catch (Exception e) {
                 throw toRuntimeException(e);
             }
@@ -97,19 +158,43 @@ public final class ExceptionalWrapper {
     }
 
     public static <T, E extends Exception> @NonNull BinaryOperator<T> wrap(@NonNull ExceptionalBinaryOperator<T, E> binaryOperator) {
-        return wrap((ExceptionalBiFunction<T, T, T, E>) binaryOperator)::apply;
+        return (t, u) -> {
+            try {
+                return binaryOperator.apply(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull DoubleBinaryOperator wrap(@NonNull ExceptionalDoubleBinaryOperator<E> binaryOperator) {
-        return wrap((ExceptionalBiFunction<Double, Double, Double, E>) binaryOperator::applyAsDouble)::apply;
+        return (t, u) -> {
+            try {
+                return binaryOperator.applyAsDouble(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull IntBinaryOperator wrap(@NonNull ExceptionalIntBinaryOperator<E> binaryOperator) {
-        return wrap((ExceptionalBiFunction<Integer, Integer, Integer, E>) binaryOperator::applyAsInt)::apply;
+        return (t, u) -> {
+            try {
+                return binaryOperator.applyAsInt(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull LongBinaryOperator wrap(@NonNull ExceptionalLongBinaryOperator<E> binaryOperator) {
-        return wrap((ExceptionalBiFunction<Long, Long, Long, E>) binaryOperator::applyAsLong)::apply;
+        return (t, u) -> {
+            try {
+                return binaryOperator.applyAsLong(t, u);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <T, E extends Exception> @NonNull Consumer<T> wrap(@NonNull ExceptionalConsumer<? super T, E> consumer) {
@@ -123,15 +208,33 @@ public final class ExceptionalWrapper {
     }
 
     public static <E extends Exception> @NonNull DoubleConsumer wrap(@NonNull ExceptionalDoubleConsumer<E> consumer) {
-        return wrap((ExceptionalConsumer<Double, E>) consumer::accept)::accept;
+        return t -> {
+            try {
+                consumer.accept(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull IntConsumer wrap(@NonNull ExceptionalIntConsumer<E> consumer) {
-        return wrap((ExceptionalConsumer<Integer, E>) consumer::accept)::accept;
+        return t -> {
+            try {
+                consumer.accept(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull LongConsumer wrap(@NonNull ExceptionalLongConsumer<E> consumer) {
-        return wrap((ExceptionalConsumer<Long, E>) consumer::accept)::accept;
+        return t -> {
+            try {
+                consumer.accept(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <T, R, E extends Exception> @NonNull Function<T, R> wrap(@NonNull ExceptionalFunction<? super T, ? extends R, E> function) {
@@ -145,51 +248,123 @@ public final class ExceptionalWrapper {
     }
 
     public static <R, E extends Exception> @NonNull DoubleFunction<R> wrap(@NonNull ExceptionalDoubleFunction<? extends R, E> function) {
-        return wrap((ExceptionalFunction<Double, R, E>) function::apply)::apply;
+        return t -> {
+            try {
+                return function.apply(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <R, E extends Exception> @NonNull IntFunction<R> wrap(@NonNull ExceptionalIntFunction<? extends R, E> function) {
-        return wrap((ExceptionalFunction<Integer, R, E>) function::apply)::apply;
+        return t -> {
+            try {
+                return function.apply(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <R, E extends Exception> @NonNull LongFunction<R> wrap(@NonNull ExceptionalLongFunction<? extends R, E> function) {
-        return wrap((ExceptionalFunction<Long, R, E>) function::apply)::apply;
+        return t -> {
+            try {
+                return function.apply(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <T, E extends Exception> @NonNull ToDoubleFunction<T> wrap(@NonNull ExceptionalToDoubleFunction<? super T, E> function) {
-        return wrap((ExceptionalFunction<T, Double, E>) function::applyAsDouble)::apply;
+        return t -> {
+            try {
+                return function.applyAsDouble(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull IntToDoubleFunction wrap(@NonNull ExceptionalIntToDoubleFunction<E> function) {
-        return wrap((ExceptionalFunction<Integer, Double, E>) function::applyAsDouble)::apply;
+        return t -> {
+            try {
+                return function.applyAsDouble(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull LongToDoubleFunction wrap(@NonNull ExceptionalLongToDoubleFunction<E> function) {
-        return wrap((ExceptionalFunction<Long, Double, E>) function::applyAsDouble)::apply;
+        return t -> {
+            try {
+                return function.applyAsDouble(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <T, E extends Exception> @NonNull ToIntFunction<T> wrap(@NonNull ExceptionalToIntFunction<? super T, E> function) {
-        return wrap((ExceptionalFunction<T, Integer, E>) function::applyAsInt)::apply;
+        return t -> {
+            try {
+                return function.applyAsInt(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull DoubleToIntFunction wrap(@NonNull ExceptionalDoubleToIntFunction<E> function) {
-        return wrap((ExceptionalFunction<Double, Integer, E>) function::applyAsInt)::apply;
+        return t -> {
+            try {
+                return function.applyAsInt(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull LongToIntFunction wrap(@NonNull ExceptionalLongToIntFunction<E> function) {
-        return wrap((ExceptionalFunction<Long, Integer, E>) function::applyAsInt)::apply;
+        return t -> {
+            try {
+                return function.applyAsInt(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <T, E extends Exception> @NonNull ToLongFunction<T> wrap(@NonNull ExceptionalToLongFunction<? super T, E> function) {
-        return wrap((ExceptionalFunction<T, Long, E>) function::applyAsLong)::apply;
+        return t -> {
+            try {
+                return function.applyAsLong(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull DoubleToLongFunction wrap(@NonNull ExceptionalDoubleToLongFunction<E> function) {
-        return wrap((ExceptionalFunction<Double, Long, E>) function::applyAsLong)::apply;
+        return t -> {
+            try {
+                return function.applyAsLong(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull IntToLongFunction wrap(@NonNull ExceptionalIntToLongFunction<E> function) {
-        return wrap((ExceptionalFunction<Integer, Long, E>) function::applyAsLong)::apply;
+        return t -> {
+            try {
+                return function.applyAsLong(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <T, E extends Exception> @NonNull Predicate<T> wrap(@NonNull ExceptionalPredicate<? super T, E> predicate) {
@@ -203,15 +378,33 @@ public final class ExceptionalWrapper {
     }
 
     public static <E extends Exception> @NonNull DoublePredicate wrap(@NonNull ExceptionalDoublePredicate<E> predicate) {
-        return wrap((ExceptionalPredicate<Double, E>) predicate::test)::test;
+        return t -> {
+            try {
+                return predicate.test(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull IntPredicate wrap(@NonNull ExceptionalIntPredicate<E> predicate) {
-        return wrap((ExceptionalPredicate<Integer, E>) predicate::test)::test;
+        return t -> {
+            try {
+                return predicate.test(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull LongPredicate wrap(@NonNull ExceptionalLongPredicate<E> predicate) {
-        return wrap((ExceptionalPredicate<Long, E>) predicate::test)::test;
+        return t -> {
+            try {
+                return predicate.test(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull Runnable wrap(@NonNull ExceptionalRunnable<E> runnable) {
@@ -235,35 +428,83 @@ public final class ExceptionalWrapper {
     }
 
     public static <E extends Exception> @NonNull BooleanSupplier wrap(@NonNull ExceptionalBooleanSupplier<E> supplier) {
-        return wrap((ExceptionalSupplier<Boolean, E>) supplier::getAsBoolean)::get;
+        return () -> {
+            try {
+                return supplier.getAsBoolean();
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull DoubleSupplier wrap(@NonNull ExceptionalDoubleSupplier<E> supplier) {
-        return wrap((ExceptionalSupplier<Double, E>) supplier::getAsDouble)::get;
+        return () -> {
+            try {
+                return supplier.getAsDouble();
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull IntSupplier wrap(@NonNull ExceptionalIntSupplier<E> supplier) {
-        return wrap((ExceptionalSupplier<Integer, E>) supplier::getAsInt)::get;
+        return () -> {
+            try {
+                return supplier.getAsInt();
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull LongSupplier wrap(@NonNull ExceptionalLongSupplier<E> supplier) {
-        return wrap((ExceptionalSupplier<Long, E>) supplier::getAsLong)::get;
+        return () -> {
+            try {
+                return supplier.getAsLong();
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <T, E extends Exception> @NonNull UnaryOperator<T> wrap(@NonNull ExceptionalUnaryOperator<T, E> unaryOperator) {
-        return wrap((ExceptionalFunction<T, T, E>) unaryOperator)::apply;
+        return t -> {
+            try {
+                return unaryOperator.apply(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull DoubleUnaryOperator wrap(@NonNull ExceptionalDoubleUnaryOperator<E> unaryOperator) {
-        return wrap((ExceptionalFunction<Double, Double, E>) unaryOperator::applyAsDouble)::apply;
+        return t -> {
+            try {
+                return unaryOperator.applyAsDouble(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull IntUnaryOperator wrap(@NonNull ExceptionalIntUnaryOperator<E> unaryOperator) {
-        return wrap((ExceptionalFunction<Integer, Integer, E>) unaryOperator::applyAsInt)::apply;
+        return t -> {
+            try {
+                return unaryOperator.applyAsInt(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     public static <E extends Exception> @NonNull LongUnaryOperator wrap(@NonNull ExceptionalLongUnaryOperator<E> unaryOperator) {
-        return wrap((ExceptionalFunction<Long, Long, E>) unaryOperator::applyAsLong)::apply;
+        return t -> {
+            try {
+                return unaryOperator.applyAsLong(t);
+            } catch (Exception e) {
+                throw toRuntimeException(e);
+            }
+        };
     }
 
     /* Specialized method names for easier use. */
