ae1a2603f50869ed2a41dc2ffb3722e046e365c8
Peter Schlömer (dapete) <dev@dapete.net>
2025-12-22 19:45:08 +0100

    Tests: call wrap for all functional interfaces

--- src/test/java/net/dapete/exceptional/ExceptionalWrapperTest.java
+++ src/test/java/net/dapete/exceptional/ExceptionalWrapperTest.java
@@ -6,13 +6,33 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
 
 class ExceptionalWrapperTest {
 
     @ParameterizedTest
     @ArgumentsSource(AllExceptionalFunctionalInterfacesArgumentsProvider.class)
-    void checkIsWrapImplementedForAllExceptionalFunctionalInterfaces(Class<?> clazz) throws NoSuchMethodException {
-        assertNotNull(ExceptionalWrapper.class.getMethod("wrap", clazz));
+    void wrapForAllExceptionalFunctionalInterfaces(Class<?> clazz) throws Exception {
+
+        // method exists (would throw a NoSuchMethodException otherwise)
+        final var method = ExceptionalWrapper.class.getMethod("wrap", clazz);
+
+        // return type matches expected type
+        final var expectedReturnType = switch (clazz.getSimpleName()) {
+            case "ExceptionalRunnable" -> Runnable.class;
+            default -> {
+                final var expectedClassName = "java.util.function." + clazz.getSimpleName().replace("Exceptional", "");
+                yield Class.forName(expectedClassName);
+            }
+        };
+        assertEquals(expectedReturnType, method.getReturnType());
+
+        // call works and returns a non-null object
+        final var mockedParameter = mock(clazz, RETURNS_DEEP_STUBS);
+        final var result = method.invoke(ExceptionalWrapper.class, mockedParameter);
+        assertNotNull(result);
+
     }
 
     @Test
