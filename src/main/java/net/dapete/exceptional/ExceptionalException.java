69408cf6817b8a85bb8af3d64043a14d6c19b72c
Peter Schlömer (dapete) <dev@dapete.net>
2025-12-24 20:53:44 +0100

    Add serialVersionUID

--- src/main/java/net/dapete/exceptional/ExceptionalException.java
+++ src/main/java/net/dapete/exceptional/ExceptionalException.java
@@ -2,12 +2,17 @@ package net.dapete.exceptional;
 
 import org.jspecify.annotations.NonNull;
 
+import java.io.Serial;
+
 /**
  * Exception that wraps exceptions returned by the functional interfaces in the {@link net.dapete.exceptional.function} package.
  * <p>
  * Used by {@link ExceptionalWrapper}.
  */
 public class ExceptionalException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 8555447007140970731L;
 
     ExceptionalException(@NonNull Exception cause) {
         super(cause);
