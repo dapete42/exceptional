9ea9544a30611af9cfb760fc21907cdf7c747e36
Peter Schlömer (dapete) <dev@dapete.net>
2025-12-25 23:10:48 +0100

    Javadoc: Fix error and rewrite package-info for net.dapete.exceptional.stream

--- src/main/java/net/dapete/exceptional/stream/package-info.java
+++ src/main/java/net/dapete/exceptional/stream/package-info.java
@@ -1,11 +1,10 @@
 /**
  * This package provides utilities for handling checked exceptions in Streams.
  * <p>
- * The main component is:
- * <ul>
- *   <li>{@link net.dapete.exceptional.stream.ExceptionalStream}:
- *   A wrapper around {@link java.util.stream.Stream} that supports equivalents to operations like {@code map}, {@code filter} or {@code forEach} (named
- *   {@code exceptionaMap, {@code exceptiona} etc.).</li>
- * </ul>
+ * The main component is {@link net.dapete.exceptional.stream.ExceptionalStream}, a wrapper around {@link java.util.stream.Stream} that supports equivalents to
+ * operations like {@code map}, {@code filter} or {@code forEach} throwing checked exceptions, named {@code exceptionalMap}, {@code exceptionalFilter} etc.
+ * <p>
+ * Similarly, there are also {@link net.dapete.exceptional.stream.ExceptionalDoubleStream}, {@link net.dapete.exceptional.stream.ExceptionalIntStream} and
+ * {@link net.dapete.exceptional.stream.ExceptionalLongStream}.
  */
 package net.dapete.exceptional.stream;
