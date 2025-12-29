854ebc13dd6085a2e306577b2dde46bc65b7eef3
Peter Schlömer (dapete) <dev@dapete.net>
2025-12-29 23:11:30 +0100

    Tests: increase line coverage of functional interfaces

--- src/test/java/net/dapete/exceptional/test/AllExceptionalFunctionalInterfacesTest.java
+++ src/test/java/net/dapete/exceptional/test/AllExceptionalFunctionalInterfacesTest.java
@@ -1,14 +1,19 @@
 package net.dapete.exceptional.test;
 
 import net.dapete.exceptional.function.Wrappable;
+import net.dapete.exceptional.stream.ExceptionalStream;
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.mockito.Answers;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.RETURNS_DEFAULTS;
 import static org.mockito.Mockito.mock;
 
 public class AllExceptionalFunctionalInterfacesTest {
@@ -20,23 +25,59 @@ public class AllExceptionalFunctionalInterfacesTest {
 
     @ParameterizedTest
     @ArgumentsSource(AllExceptionalFunctionalInterfacesArgumentsProvider.class)
-    void wrapForAllExceptionalFunctionalInterfaces(Class<?> clazz) throws Exception {
+    void wrap(Class<?> clazz) throws Exception {
 
         // implements Wrappable
         assertTrue(Arrays.asList(clazz.getInterfaces()).contains(Wrappable.class));
 
-        // method exists (would throw a NoSuchMethodException otherwise)
-        final var method = clazz.getMethod("wrap");
+        // method wrap() exists (would throw a NoSuchMethodException otherwise) and has no parameters
+        final var wrapMethod = clazz.getMethod("wrap");
+        assertEquals(0, wrapMethod.getParameterCount());
 
         // return type matches expected type
-        final var expectedReturnType = AllExceptionalFunctionalInterfacesArgumentsProvider.getExpectedReturnType(clazz);
-        assertEquals(expectedReturnType, method.getReturnType());
+        final var returnType = AllExceptionalFunctionalInterfacesArgumentsProvider.getExpectedReturnType(clazz);
+        assertEquals(returnType, wrapMethod.getReturnType());
 
-        // call works and returns a non-null object
-        final var mockedInstance = mock(clazz, RETURNS_DEEP_STUBS);
-        final var result = method.invoke(mockedInstance);
-        assertNotNull(result);
+        // call the method on a mock calling real methods
+        final var exceptionalInterfaceMock = mock(clazz, Answers.CALLS_REAL_METHODS);
+        final var functionalInterfaceResult = wrapMethod.invoke(exceptionalInterfaceMock);
+        assertInstanceOf(returnType, functionalInterfaceResult);
 
+        // call functionalInterfaceResult.<theFunctionalInterfaceMethod> (run, get, apply, etc.)
+        final var functionalInterfaceMethod = findFunctionalInterfaceMethod(returnType);
+        final var mockedParameters = mockParameters(functionalInterfaceMethod);
+        functionalInterfaceMethod.invoke(functionalInterfaceResult, mockedParameters);
+
+    }
+
+    private static @NonNull Method findFunctionalInterfaceMethod(Class<?> clazz) {
+        // a functional interface can only have one non-default method
+        return Stream.of(clazz.getMethods())
+                .filter(m -> !m.isDefault())
+                .findAny()
+                .orElseThrow();
+    }
+
+    private static Object @NonNull [] mockParameters(Method method) {
+        // mock all parameters (or use defaults for primitives)
+        return ExceptionalStream.of(method.getParameters())
+                .exceptionalMap(p -> {
+                    if (p.getType().isPrimitive()) {
+                        return switch (p.getType().getName()) {
+                            case "boolean" -> false;
+                            case "byte" -> (byte) 0;
+                            case "char" -> (char) 0;
+                            case "double" -> 0d;
+                            case "float" -> 0f;
+                            case "int" -> 0;
+                            case "long" -> 0L;
+                            default -> throw new RuntimeException(p.getType().toString());
+                        };
+                    } else {
+                        return mock(p.getType(), RETURNS_DEFAULTS);
+                    }
+                })
+                .toArray();
     }
 
 }
